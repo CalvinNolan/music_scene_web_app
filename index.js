150115a6511ebb422e2720a329d37ddadec88e5b
kaner94 <kanery@tcd.ie>
2016-03-05 18:40:28 +0000

    Fix background visibility. Add 404 error.

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.get('/', function(req, res) {
 	res.render('index.html');
 });
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('404: This page could not be found!');
+  err.status = 404;
+  next(err);
+});
+
 app.listen(3030, function () {
   console.log('Example app listening on port 3030!');
 });
