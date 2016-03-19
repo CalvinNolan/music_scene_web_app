9f102515dbf0001b02b8cb36b870eb2c49a4c43d
Calvin Nolan <Calvinnolan@gmail.com>
2016-03-19 13:47:59 +0000

    Working playlist generation
    Make sure you have the music_scene_api server running on port 3000 for
    it to work!

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
+app.engine('html', require('ejs').renderFile);
 
 //Html redirect requests.
 app.get('/', function(req, res) {
 	res.render('index.html');
 });
+
+app.get('/playlist', function(req, res) {
+	res.render('playlist.html');
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
